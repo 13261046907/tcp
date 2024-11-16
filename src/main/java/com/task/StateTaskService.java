40e37363665ca5a8481ae15f3001546588b44e60
yangtao <yangtao@insshopy.com>
2024-11-16 19:47:46 +0800

    定时任务修改

--- src/main/java/com/task/StateTaskService.java
+++ src/main/java/com/task/StateTaskService.java
@@ -38,10 +38,12 @@ public class StateTaskService {
         if(CollectionUtil.isNotEmpty(deviceModelVos)){
             deviceModelVos.stream().forEach(deviceModelVo -> {
                 String deviceId = deviceModelVo.getDeviceId();
-                DeviceModel deviceModel = deviceInstanceService.selectDeviceModelByChannelId(null, deviceModelVo.getImei());
+                String modelId = deviceModelVo.getModelId();
+                DeviceModel deviceModel = deviceInstanceService.selectDeviceModelByChannelId(null, modelId);
                 if(!Objects.isNull(deviceModel)){
+                    String imei = deviceModel.getImei();
                     try {
-                        log.info("设备ID:deviceId:{},channel:{},发送指令:{}",deviceId,deviceModel.getChannel(),deviceModelVo.getInstructionCrc());
+                        log.info("IMEI:{},设备ID:deviceId:{},channel:{},发送指令:{}",modelId,deviceId,deviceModel.getChannel(),deviceModelVo.getInstructionCrc());
                         //判断当前4g模块心跳时间大于2分钟说明掉线
                         Date createTime = deviceModel.getCreateTime();
                         // 将 createTime 转换为 Instant
@@ -51,19 +53,21 @@ public class StateTaskService {
                         Duration duration = Duration.between(createTimeInstant, currentTime);
                         // 判断差值是否大于两分钟
                         if (duration.toMinutes() > 5) {
-                            log.info("设备ID:deviceId:{},设备掉线",deviceId);
+                            log.info("IMEI:{},4G掉线,删除数据",imei);
                             deviceInstanceService.deleteDeviceModelByChannelId(deviceModel.getChannel());
                             //修改设备和产品下线
                             List<String> deviceIdList = deviceInstanceService.selectDeviceIdByModelId(deviceModel.getModelId());
                             if(!CollectionUtils.isEmpty(deviceIdList)){
                                 deviceIdList.stream().forEach(queryDeviceId ->{
+                                    log.info("设备ID:deviceId:{},设备掉线",queryDeviceId);
                                     deviceInstanceService.updateDeviceStateByDeviceId(DeviceStateEnum.offline.getValue(),queryDeviceId);
                                     String productId = deviceInstanceService.selectProductIdByDeviceId(queryDeviceId);
+                                    log.info("产品ID:deviceId:{},设备掉线",productId);
                                     deviceInstanceService.updateProductStateByProductId(DeviceStateEnum.offline.getName(),productId);
                                 });
                             }
                         } else {
-                            log.info("设备ID:deviceId:{},设备在线",deviceId);
+                            log.info("4G在线:{}",imei);
                         }
 //                        serverHandler.syncChannelWrite(deviceModel.getChannel(), deviceModelVo.getInstructionCrc());
                     } catch (Exception e) {
@@ -71,8 +75,10 @@ public class StateTaskService {
                     }
                 }else {
                     //设备下线
-                    log.info("设备ID:deviceId:{},下线",deviceId);
+                    log.info("deviceId:{},下线",deviceId);
                     deviceInstanceService.updateDeviceStateByDeviceId(DeviceStateEnum.offline.getValue(),deviceId);
+                    log.info("产品ID:IMEI:{},下线",modelId);
+                    deviceInstanceService.updateProductStateByProductId(DeviceStateEnum.offline.getName(),modelId);
                 }
             });
         }
